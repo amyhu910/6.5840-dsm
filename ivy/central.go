049e062604dc2dd7e001da4009b5ea961965e54b
Amy Hu <amyhu@mit.edu>
2024-04-22 11:43:30 -0400

    baseline code

--- ivy/central.go
+++ ivy/central.go
@@ -1,19 +1,68 @@
 package ivy
 
+import "6.5840-dsm/labrpc"
+
 type Central struct {
 	// The central's name
-	copyset map[int][]int
+	clients map[int]*labrpc.ClientEnd
+	copyset map[int]map[int]int
 	owner   map[int]int
 }
 
-func (c *Central) handleRead() {
+func (c *Central) handleReadWrite(args *ReadWriteArgs, reply *ReadWriteReply) {
+	if args.Access == 1 {
+		data := c.makeReadOnly(args.PageID)
+		if _, ok := c.copyset[args.PageID]; !ok {
+			c.copyset[args.PageID] = make(map[int]int)
+		}
+		c.copyset[args.PageID][args.ClientID] = 1
+		reply.Err = OK
+		reply.Data = data
+	} else if args.Access == 2 {
+		c.invalidateCaches(args.PageID)
+		for len(c.copyset[args.PageID]) > 0 {
+		}
+		c.owner[args.PageID] = args.ClientID
+	}
 }
 
-func (c *Central) handleWrite() {
+func (c *Central) invalidateCaches(pageID int) {
+	copyset, ok := c.copyset[pageID]
+	if !ok || len(copyset) == 0 {
+		return
+	}
+	for clientID, _ := range copyset {
+		go c.makeInvalid(pageID, clientID)
+	}
 }
 
-func (c *Central) invalidateCaches() {
+func (c *Central) makeInvalid(pageID int, clientID int) {
+	args := AccessArgs{PageID: pageID, NewAccess: 0}
+	reply := AccessReply{}
+	ok := c.clients[clientID].Call("Client.ChangeAccess", &args, &reply)
+	for !ok {
+		ok = c.clients[clientID].Call("Client.ChangeAccess", &args, &reply)
+	}
+	delete(c.copyset[pageID], clientID)
+}
+
+func (c *Central) makeReadOnly(pageID int) []byte {
+	clientID, hasOwner := c.owner[pageID]
+	if !hasOwner || clientID == -1 {
+		return nil
+	}
+	args := AccessArgs{PageID: pageID, NewAccess: 1}
+	reply := AccessReply{}
+	ok := c.clients[clientID].Call("Client.ChangeAccess", &args, &reply)
+	for !ok {
+		ok = c.clients[clientID].Call("Client.ChangeAccess", &args, &reply)
+	}
+	c.owner[pageID] = -1
+	return reply.Data
 }
 
 func (c *Central) initialize() {
+	c.clients = make(map[int]*labrpc.ClientEnd)
+	c.copyset = make(map[int]map[int]int)
+	c.owner = make(map[int]int)
 }
--- ivy/client.go
+++ ivy/client.go
@@ -4,6 +4,12 @@ import (
 	"6.5840-dsm/labrpc"
 )
 
+const (
+	OK = "OK"
+)
+
+type Err string
+
 type Page struct {
 	id     int
 	data   []byte
@@ -11,36 +17,86 @@ type Page struct {
 }
 
 type Client struct {
+	central   *labrpc.ClientEnd
 	id        int
 	pagetable map[int]Page
 }
 
+type ReadWriteArgs struct {
+	ClientID int
+	PageID   int
+	Access   int
+}
+
+type ReadWriteReply struct {
+	Err  Err
+	Data []byte
+}
+
+type AccessArgs struct {
+	PageID    int
+	NewAccess int
+}
+
+type AccessReply struct {
+	Err  Err
+	Data []byte
+}
+
 func (c *Client) readPage(pageID int) []byte {
 	// check locally first, send request to central if necessary
-	return make([]byte, 0)
+	if page, ok := c.pagetable[pageID]; ok && page.access == 0 {
+		return c.pagetable[pageID].data
+	} else {
+		return c.sendReadRequest(pageID)
+	}
 }
 
-func (c *Client) sendReadRequest() {
-}
-
-func (c *Client) handleReadResponse() {
+func (c *Client) sendReadRequest(pageID int) []byte {
+	reply := &ReadWriteReply{}
+	ok := c.central.Call("Central.handleReadWrite", &ReadWriteArgs{ClientID: c.id, PageID: pageID, Access: 1}, reply)
+	if !ok {
+		return nil
+	}
+	return reply.Data
 }
 
 func (c *Client) writePage(pageID int, data []byte) {
 	// check locally first for read-write permissions,
 	// send request to central if necessary
+	if page, ok := c.pagetable[pageID]; ok && page.access == 2 {
+		page.data = data           // Assign data to a variable before updating the struct field
+		c.pagetable[pageID] = page // Update the struct field in the map
+	} else {
+		reply := c.sendWriteRequest(pageID)
+		if reply == OK {
+			c.pagetable[pageID] = Page{id: pageID, data: data, access: 2}
+		}
+	}
 }
 
-func (c *Client) sendWriteRequest() {
+func (c *Client) sendWriteRequest(pageID int) Err {
+	reply := &ReadWriteReply{}
+	ok := c.central.Call("Central.handleReadWrite", &ReadWriteArgs{ClientID: c.id, PageID: pageID, Access: 2}, reply)
+	if !ok {
+		return "Failed to write"
+	}
+	return reply.Err
 }
 
-func (c *Client) handleWriteResponse() {
+func (c *Client) ChangeAccess(args *AccessArgs, reply *AccessReply) {
+	if page, ok := c.pagetable[args.PageID]; ok {
+		page.access = args.NewAccess
+		c.pagetable[args.PageID] = page
+		reply.Err = OK
+		reply.Data = page.data
+	} else {
+		reply.Err = "Page not found"
+	}
 }
 
-func (c *Client) handleInvalidate() {
-
-}
-
-func (c *Client) initialize(server *labrpc.ClientEnd) {
-	ok := server.Call("KVServer.Get", &args, &reply)
+func (c *Client) initialize(server *labrpc.ClientEnd, me int) {
+	c.central = server
+	c.pagetable = make(map[int]Page)
+	c.id = me
 }
